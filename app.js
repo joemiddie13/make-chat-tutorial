86eff68192549cc9d12693d5ef07f24d180ca757
joemiddie13 <joemiddie13@gmail.com>
2024-06-15 17:50:25 -0700

    Integrated Socket.io

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
 
-// Updated import for express-handlebars
+// Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+});
+
+// Express View Engine for Handlebars
 const { engine } = require('express-handlebars');
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+
+// Establish your public folder
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
